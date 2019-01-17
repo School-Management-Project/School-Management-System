330787c6ff5122b4ac433633d92acf0da96c8b1a
aratijadhav <aratijadhav1610@gmail.com>
2019-01-17 20:26:31 +0530

    basic djangoapp.

--- djangoApp/CMSApp/admin.py
+++ djangoApp/CMSApp/admin.py
@@ -4,10 +4,3 @@ from .models import Student,Department,Courses,Faculty,Subject,Semester
 from .models import Parent
 
 # Register your models here.
-admin.site.register(Student)
-admin.site.register(Department)
-admin.site.register(Courses)
-admin.site.register(Faculty)
-admin.site.register(Subject)
-admin.site.register(Semester)
-admin.site.register(Parent)
--- djangoApp/CMSApp/urls.py
+++ djangoApp/CMSApp/urls.py
@@ -1,20 +1,6 @@
 from django.conf.urls import url
 from .views import StudentViewSet, index, SpecificStudentViewSet,DepartmentViewSet,CoursesViewSet,FacultyViewSet,SemesterViewSet,SubjectViewSet,SubjectFacultyViewSet
 from .views import ParentViewSet
-from rest_framework import routers
-
-router = routers.SimpleRouter()
-
-router.register(r'student',StudentViewSet)
-router.register(r'student/(?P<rollno>.+)', SpecificStudentViewSet)
-router.register(r'department',DepartmentViewSet)
-router.register(r'course',CoursesViewSet)
-router.register(r'faculty',FacultyViewSet)
-router.register(r'subject',SubjectViewSet)
-router.register(r'semester',SemesterViewSet)
-router.register(r'subjectfaculty',SubjectFacultyViewSet)
-router.register(r'parent',ParentViewSet)
-
 
 urlpatterns = router.urls
 
